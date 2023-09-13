470dcc832de4d58708b8ecbef4ddaa2b9984639e
MuhammadSafi2303KHIDEG023 <mohammad.safi@xloopdigital.com>
2023-09-13 16:26:46 +0500

    fixes pushed : local ChromDb added

--- app.py
+++ app.py
@@ -7,11 +7,22 @@ from llama_index.llms import OpenAI
 from llama_index.indices.postprocessor import MetadataReplacementPostProcessor
 from llama_index.node_parser import SentenceWindowNodeParser
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index import (
+    VectorStoreIndex,
+    ServiceContext,
+    StorageContext,
+    load_index_from_storage,
+)
 from llama_index.llms import OpenAI
+from llama_index.storage.docstore import SimpleDocumentStore
+from llama_index.storage.index_store import SimpleIndexStore
+from llama_index.vector_stores import SimpleVectorStore, ChromaVectorStore
+from llama_index.node_parser import SimpleNodeParser
 from langchain.vectorstores import FAISS
-
+from llama_index.vector_stores.faiss import FaissVectorStore
+import faiss
 import openai
+import chromadb
 
 os.environ["OPENAI_API_KEY"] = "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"
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -30,7 +41,7 @@ if "messages" not in st.session_state.keys():  # Initialize the chat message his
 
 
 @st.cache_resource(show_spinner=False)
-def load_data():
+def load_data(folder_path):
     with st.spinner(
         text="Loading and indexing the GE docs – hang tight! This should take 1-2 minutes."
     ):
@@ -39,13 +50,14 @@ def load_data():
 
         node_parser = SentenceWindowNodeParser.from_defaults(
             # how many sentences on either side to capture
-            window_size=3,
+            window_size=2,
             # the metadata key that holds the window of surrounding sentences
             window_metadata_key="window",
             # the metadata key that holds the original sentence
             original_text_metadata_key="original_sentence",
         )
         llm = OpenAI(model="gpt-4", temperature=0)
+
         service_context = ServiceContext.from_defaults(
             llm=llm,
             embed_model=HuggingFaceEmbeddings(
@@ -53,18 +65,57 @@ def load_data():
             ),
             node_parser=node_parser,
         )
-        # db = FAISS.from_documents(docs, service_context)
-        # db.save_local(DB_FAISS_PATH)
 
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        # chroma
+        db = chromadb.PersistentClient(path="./chroma_db")
+        chroma_collection = db.get_or_create_collection("quickstart")
+
+        vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+        index = VectorStoreIndex.from_documents(
+            docs, service_context=service_context, storage_context=storage_context
+        )
+
         return index
 
 
-index = load_data()
+def retrieve_data():
+    db2 = chromadb.PersistentClient(path="./chroma_db")
+    chroma_collection = db2.get_or_create_collection("quickstart")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+    node_parser = SentenceWindowNodeParser.from_defaults(
+        # how many sentences on either side to capture
+        window_size=2,
+        # the metadata key that holds the window of surrounding sentences
+        window_metadata_key="window",
+        # the metadata key that holds the original sentence
+        original_text_metadata_key="original_sentence",
+    )
+    llm = OpenAI(model="gpt-4", temperature=0)
+
+    service_context = ServiceContext.from_defaults(
+        llm=llm,
+        embed_model=HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        ),
+        node_parser=node_parser,
+    )
+    index = VectorStoreIndex.from_vector_store(
+        vector_store, service_context=service_context
+    )
+    return index
+
+
+folder_path = "chroma_db"
+
+index = retrieve_data() if os.path.exists(folder_path) else load_data(folder_path)
+
 
 # chat_engine = index.as_chat_engine(chat_mode="react", verbose=True)
 query_engine = index.as_query_engine(
-    similarity_top_k=4,
+    similarity_top_k=6,
     # the target key defaults to `window` to match the node_parser's default
     node_postprocessors=[
         MetadataReplacementPostProcessor(target_metadata_key="window")
